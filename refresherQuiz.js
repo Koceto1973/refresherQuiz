ce710586185fe9d29576adbafa7ca2a4e2c3ecd4
TeamK <koceto1973@yahoo.ca>
2018-12-10 21:49:58 +0200

    styling refactor twice

--- refresherQuiz.js
+++ refresherQuiz.js
@@ -1,46 +1,45 @@
 // IIFE
 (function() {
-    // questions database
-    var questions = [];
-
+    
     var Question = function(number,text,answers,rightAnswer){
         this.number = number;
         this.text = text;
         this.answers = answers;
         this.rightAnswer = rightAnswer;
-        this.show = function() {            
-            // format prompt
-            var boxMessage = "                  Quiz question: \n";
-            boxMessage += " "+this.text+"\n";
-            for (i=0; i<this.answers.length; i++) {
-                boxMessage += "     "+(i+1)+"."+this.answers[i]+" \n";
-            }
-            boxMessage += "\n Note: Enter 0 to exit the quiz ...";
+    }
 
-            // display prompt and get answer
-            var answer = prompt(boxMessage);
-            
-            if ( answer == 0 ) { 
-                inQuizMood = false; 
-            } else {
-                if ( this.rightAnswer == answer ) {
-                    truthies += 1;
-                    console.log("Right! You scored: "+ truthies + " of total " + (truthies+falsies) + " questions." ); 
-                } else {
-                    falsies += 1;
-                    console.log("Wrong! You scored: "+ truthies + " of total " + (truthies+falsies) + " questions." ); 
-                }                
-            } 
+    Question.prototype.promptQuestion = function() {
+        // format prompt
+        var boxMessage = "                  Quiz question: \n";
+        boxMessage += " "+this.text+"\n";
+        for (i=0; i<this.answers.length; i++) {
+            boxMessage += "     "+(i+1)+"."+this.answers[i]+" \n";
         }
+        boxMessage += "\n Note: type exit to quit the quiz ...";
+
+        // display prompt and get answer
+        var answer = prompt(boxMessage);
+        
+        if ( answer === 'exit' ) { 
+            inQuizMood = false; 
+        } else {
+            if ( this.rightAnswer == answer ) {
+                truthies += 1;
+                console.log("Right! You scored: "+ truthies + " of total " + (truthies+falsies) + " questions." ); 
+            } else {
+                falsies += 1;
+                console.log("Wrong! You scored: "+ truthies + " of total " + (truthies+falsies) + " questions." ); 
+            }                
+        } 
     }
 
     // pushing couple questions
     var q1 = new Question(1,"What is the most popular web programming language?",["JavaScript","C#","Java"],1);
-    questions.push(q1);
     var q2 = new Question(2,"Is number a value type in JavaScript?",["Yes","No"],1);
-    questions.push(q2);
     var q3 = new Question(3,"What is the server side alternative of JavaScript?",["Go","C++","Node","Java"],3);
-    questions.push(q3);
+    
+    // questions database
+    var questions = [q1,q2,q3];
 
     // random question number generator
     function PickRandomQuestionNumber(questionsBase) {
@@ -54,6 +53,6 @@
     while (inQuizMood) {
         // pick random number
         var x = PickRandomQuestionNumber(questions)
-        questions[x].show();        
+        questions[x].promptQuestion();        
     }    
 })() 
