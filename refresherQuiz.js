be79c90056d943dff0d760c96458927dd37fc65a
TeamK <koceto1973@yahoo.ca>
2018-12-10 21:20:51 +0200

    styling refactor once

--- refresherQuiz.js
+++ refresherQuiz.js
@@ -2,14 +2,39 @@
 (function() {
     // questions database
     var questions = [];
+
     var Question = function(number,text,answers,rightAnswer){
         this.number = number;
         this.text = text;
         this.answers = answers;
         this.rightAnswer = rightAnswer;
+        this.show = function() {            
+            // format prompt
+            var boxMessage = "                  Quiz question: \n";
+            boxMessage += " "+this.text+"\n";
+            for (i=0; i<this.answers.length; i++) {
+                boxMessage += "     "+(i+1)+"."+this.answers[i]+" \n";
+            }
+            boxMessage += "\n Note: Enter 0 to exit the quiz ...";
+
+            // display prompt and get answer
+            var answer = prompt(boxMessage);
+            
+            if ( answer == 0 ) { 
+                inQuizMood = false; 
+            } else {
+                if ( this.rightAnswer == answer ) {
+                    truthies += 1;
+                    console.log("Right! You scored: "+ truthies + " of total " + (truthies+falsies) + " questions." ); 
+                } else {
+                    falsies += 1;
+                    console.log("Wrong! You scored: "+ truthies + " of total " + (truthies+falsies) + " questions." ); 
+                }                
+            } 
+        }
     }
 
-    // pushing more questions
+    // pushing couple questions
     var q1 = new Question(1,"What is the most popular web programming language?",["JavaScript","C#","Java"],1);
     questions.push(q1);
     var q2 = new Question(2,"Is number a value type in JavaScript?",["Yes","No"],1);
@@ -29,29 +54,6 @@
     while (inQuizMood) {
         // pick random number
         var x = PickRandomQuestionNumber(questions)
-
-        // format prompt
-        var boxMessage = "                  Quiz question: \n";
-        boxMessage += " "+questions[x].text+"\n";
-        for (i=0; i<questions[x].answers.length; i++) {
-            boxMessage += "     "+(i+1)+"."+questions[x].answers[i]+" \n";
-        }
-        boxMessage += "\n Note: Enter 0 to exit the quiz ...";
-
-        // display prompt and get answer
-        var answer = prompt(boxMessage);
-        
-        if ( answer == 0 ) { 
-            inQuizMood = false; 
-        } else {
-            if ( questions[x].rightAnswer == answer ) {
-                truthies += 1;
-                console.log("Right! You scored: "+ truthies + " of total " + (truthies+falsies) + " questions." ); 
-            } else {
-                falsies += 1;
-                console.log("Wrong! You scored: "+ truthies + " of total " + (truthies+falsies) + " questions." ); 
-            }
-               
-        } 
+        questions[x].show();        
     }    
 })() 
